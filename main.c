1246fe992acd5420caf2b04a97ca14a483245f64
Jaturong Kongmanee <jaturongkongmanee@Jaturongs-MacBook-Pro.local>
2019-03-19 17:00:55 -0500

    Calculate the avg. of rrt, successful, and failed transmission

--- main.c
+++ main.c
@@ -21,6 +21,7 @@ struct msg {
     long to;
     TIME start_time;
     msg_t link;
+    long count;
 };
 
 msg_t msg_queue;
@@ -44,14 +45,13 @@ msg_t new_msg();
 void count_msg();
 
 
-long avg_suc_trans, avg_failed_tran, rtt;
+long suc_trans, failed_tran, rtt;
+long total_trans;
 double loss_prob_threshold = 0.3;
 long num_gen_mes_node1, num_gen_mes_node2, num_gen_mes_node3;
 long num_gen_mes_node4, num_gen_mes_node0;
 
 void sim(){
-    
-
     printf( "Enter a loss probability threshold\n:");
     scanf("%lf", &loss_prob_threshold);
     printf( "You entered loss proability threshold: %lf\n", loss_prob_threshold);
@@ -61,10 +61,17 @@ void sim(){
     hold(SIMTIME);
 
 
-    printf("Average number of successful transmissions %ld\n", avg_suc_trans);
-    printf("Average number of failed transmissions %ld\n", avg_failed_tran);
-    printf("Average roundtrip time %ld\n", rtt);
-    /*printf("Count %ld\n", count);*/
+    printf("Total number of successful transmissions %ld\n", suc_trans);
+    printf("Total number of failed transmissions %ld\n", failed_tran);
+    printf("Total roundtrip time %ld\n", rtt);
+    printf("Total transmission %ld\n", total_trans);
+
+
+    printf("Average number of successful transmissions %.2f\n", suc_trans/(float)total_trans);
+    printf("Average number of failed transmissions %.2f\n", failed_tran/(float)total_trans);
+    printf("Average roundtrip time %.2f\n", rtt/(float)total_trans);
+
+
     printf("Node 0 gen %ld\n", num_gen_mes_node0);
     printf("Node 1 gen %ld\n", num_gen_mes_node1);
     printf("Node 2 gen %ld\n", num_gen_mes_node2);
@@ -105,44 +112,63 @@ void init(){
 
 void proc(long n){
     msg_t m;
-    long s, t;
-    
+    long s, t;  
 
+    /* every node start to send a packet when its process is initiated*/
     create("proc");
+    hold(exponential(INTER_ARR_TIME));
+    m = new_msg(n);
+    m->count = 1;
+    send_msg(m);
+    printf("----------------");
+    printf("node.%ld sends a HELLO to node.%ld at %6.3f seconds\n", m->from, m->to, clock);
     while(clock < SIMTIME){
-        hold(exponential(INTER_ARR_TIME));
-        /*use(node[n].cpu, exponential(INTER_ARR_TIME));*/
-        m = new_msg(n);
-        send_msg(m);
-        printf("node.%ld sends a HELLO to node.%ld at %6.3f seconds\n", m->from, m->to, clock);
-        count_msg("gen msg", m);
         
-
         s = timed_receive(node[n].mbox, (long *)&m, TIME_OUT);
         switch(s){
             case TIMED_OUT:
-                avg_failed_tran += 1;
-                rtt += 2;
-                send_msg(m);
-                printf("node.%ld re-sends a HELLO to node.%ld at %6.3f seconds\n", m->from, m->to, clock);
+                /* if the sender doe not receive ack packet after sending two times --> it's failed, generate a new packet */
+                if (m->count >= 2.0) {
+                    hold(exponential(INTER_ARR_TIME));
+                    m = new_msg(n);
+                    m->count = 1;
+                    send_msg(m);
+                    printf("node.%ld sends a HELLO to node.%ld at %6.3f seconds\n", m->from, m->to, clock);
+                    /*printf("msg count %ld", m->count);*/
+                    count_msg("gen msg", m);
+
+                    failed_tran += 1;
+                    total_trans += 1;
+                } else
+                {                  
+                    rtt += 2;
+                    m->count += 1.0;
+                    printf("msg count %ld\n", m->count);
+                    send_msg(m);
+                    printf("node.%ld re-sends a HELLO to node.%ld at %6.3f seconds\n", m->from, m->to, clock);
+                }
+                
                 break;
             case EVENT_OCCURRED:
                 t = m->type;
                 switch(t){
                 case REQUEST:
+                    /* it would take two seconds */
                     form_reply(m);
                     double loss_prob = uniform(0.0, 1.0);
                     if(loss_prob > loss_prob_threshold ){
+                        printf("node.%ld replies a HELLO_ACK to node.%ld at %6.3f seconds\n", m->from, m->to, clock);    
                         send_msg(m);
                     }
                     float clock1 = clock;
-                    printf("node.%ld replies a HELLO_ACK to node.%ld at %6.3f seconds\n", m->from, m->to, clock1);           
+                           
                     break;
                 case REPLY:
                     record(clock - m->start_time, resp_tm);
                     return_msg(m);
-                    avg_suc_trans += 1;
+                    suc_trans += 1;
                     rtt += 0.4;
+                    total_trans += 1;
                     printf("node.%ld receives a HELLO_ACK from node.%ld at %6.3f seconds\n", m->from, m->to, clock);
                     break;
                 default:
